22f94ac22a9d3ba57e9dd6d870f5f100ca240d0d
persephone-svai <persephone@smartverify.ai>
2026-09-21 09:55:16 -0700

    feat: add postgres connection

--- mcp_server.py
+++ mcp_server.py
@@ -1,9 +1,32 @@
 from fastmcp import FastMCP
 import psycopg2
 import psycopg2.extras
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+
+print("connecting to the database")
+connection = psycopg2.connect(
+    host=DATABASE_HOST,
+    user=DATABASE_USER,
+    password=DATABASE_PASSWORD,
+    port=DATABASE_PORT,
+    dbname=DATABASE_NAME,
+    cursor_factory=psycopg2.extras.DictCursor
+)
+print("connected")
+cur = connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+print("Created cursor")
+print("Database setup complete")
 
 mcp = FastMCP("MCP Demo Server")
+
 
 @mcp.tool
 def example_tool():
