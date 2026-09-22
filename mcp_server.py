d7a33634b2f44b5cb8ce3d91ee8312c4c09e0848
persephone-svai <persephone@smartverify.ai>
2026-09-22 10:58:21 -0700

    fix: dotenv path

--- mcp_server.py
+++ mcp_server.py
@@ -1,3 +1,4 @@
+from anyio import Path
 from fastmcp import FastMCP
 import psycopg2
 import psycopg2.extras
@@ -6,7 +7,7 @@ import os
 import logging
 logging.basicConfig(level=logging.INFO)
 
-load_dotenv()
+load_dotenv(dotenv_path=Path(__file__).parent / ".env")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 DATABASE_HOST = os.getenv("DATABASE_HOST")
 DATABASE_USER = os.getenv("DATABASE_USER")
@@ -22,18 +23,14 @@ connection = psycopg2.connect(
     dbname=DATABASE_NAME,
     cursor_factory=psycopg2.extras.DictCursor
 )
+
+logging.info(f"host={DATABASE_HOST!r} user={DATABASE_USER!r} port={DATABASE_PORT!r} pw_set={bool(DATABASE_PASSWORD)}")
 logging.info("connected")
 cur = connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 logging.info("Created cursor")
 logging.info("Database setup complete")
 
 mcp = FastMCP("MCP Demo Server")
-
-
-@mcp.tool
-def example_tool():
-    return "This is an example tool."
-
 
 if __name__ == "__main__":
     mcp.run()
